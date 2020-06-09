0ac91e7035c075feba6ad9b6dd2d65c1d84908da
faustyn-p <faustyn.piechowiak@gmail.com>
2020-06-09 21:31:08 +0200

    interface changes

--- src/yeelight.interface.ts
+++ src/yeelight.interface.ts
@@ -1,4 +1,3 @@
-import { Socket } from 'net';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { Observable } from 'rxjs';
 
@@ -8,302 +7,186 @@ export interface IYeelightDevice {
 	 * @returns {BehaviorSubject<boolean>} Returns connected state of device.
 	 */
 	connected: BehaviorSubject<boolean>;
-	socket: Socket;
-	location: string;
-	host: string;
-	port: number;
+
+	/**
+	 * @returns {string} Unique ID of the device.
+	 */
 	id: string;
+
 	/**
 	 * @returns {BehaviorSubject<string>} Returns device friendly-name
 	 */
 	model: YeelightDeviceModelEnum;
+
 	/**
 	 * @returns {YeelightSupportedMethodsEnum[]} Returns list of supported methods for this device. If you run method, that is not listed here, error will be returned.
 	 */
 	supportedMethods: YeelightSupportedMethodsEnum[];
+
 	/**
-	 * Get propety of Yeelight Device like name or power status.
+	 * @returns {BehaviorSubject<string>} Returns device friendly-name
 	 */
-	get: {
-		/**
-		 * @returns {BehaviorSubject<string>} Returns device friendly-name
-		 */
-		name: BehaviorSubject<string>;
+	name: BehaviorSubject<string>;
 
-		/**
-		 * @returns {BehaviorSubject<YeelightPowerState>} Returns device power state - `on` or `off`
-		 */
-		power: BehaviorSubject<YeelightPowerState>;
-
-		/**
-		 * @returns {BehaviorSubject<number>} Returns brightness of the device (in percent 0 - 100)
-		 */
-		brightness?: BehaviorSubject<number>;
-
-		/**
-		 * @returns {BehaviorSubject<number>} Returns color temperature of the device (in Kelvin of range from 2700 to 6500)
-		 */
-		colorTemperature?: BehaviorSubject<number>;
-
-		/**
-		 * @returns {BehaviorSubject<number>} Returns RGB color (in RGB Hex)
-		 */
-		rgb?: BehaviorSubject<string>;
-
-		/**
-		 * @returns {BehaviorSubject<string>} Returns hue of the device (in degrees 0 - 359)
-		 */
-		hue?: BehaviorSubject<number>;
-
-		/**
-		 * @returns {BehaviorSubject<number>} Returns saturation of the device (0 - 100)
-		 */
-		saturation?: BehaviorSubject<number>;
-
-		/**
-		 * @returns {BehaviorSubject<number>} Returns color mode of the device
-		 *
-		 * `1` - rgb mode
-		 *
-		 * `2` - color temperature mode
-		 *
-		 * `3` - hsv mode
-		 */
-		colorMode?: BehaviorSubject<number>;
-	};
 	/**
-	 * Change parameters of device like power state or color
+	 * @returns {BehaviorSubject<YeelightPowerState>} Returns device power state - `on` or `off`
 	 */
-	set: {
-		/**
-		 * Set name of the device
-		 * @param {string} name New friendly-name of the device
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		name: (name: string) => Promise<IYeelightMethodResponse>;
+	power: BehaviorSubject<YeelightPowerState>;
 
-		/**
-		 * Set power of the device
-		 * @param {YeelightPowerState} power `on` or `off`
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		power: (power: YeelightPowerState, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-
-		/**
-		 * Set color temperature of the device
-		 * @param {number} colorTemperature Number in the range from 2700 to 6500 (Kelvin)
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		colorTemperature: (colorTemperature: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-
-		/**
-		 * Set RGB color of the device
-		 * @param {string | number | number[]} rgb RGB Color. It can be hex color ("#FFFFFF"), number (16777215) or array of numbers ([255, 255, 255])
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		rgb: (rgb: string | number | number[], effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-
-		/**
-		 * Set hue and saturation of the device
-		 * @param {number} hue Number in the range from 0 to 359 (degrees)
-		 * @param {number} saturation Number in the range from 0 to 100
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		hsv: (hue: number, saturation: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-
-		/**
-		 * Set brightness of the device
-		 * @param {number} brightness Number in the range from 0 to 359 (percent)
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		brightness: (brightness: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-
-		/**
-		 * Set current device state as default
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		asDefault: () => Promise<IYeelightMethodResponse>;
-	};
 	/**
-	 * Toggle current device power state.
+	 * @returns {BehaviorSubject<number>} Returns brightness of the device (in percent 0 - 100)
+	 */
+	brightness: BehaviorSubject<number>;
+
+	/**
+	 * @returns {BehaviorSubject<number>} Returns color temperature of the device (in Kelvin of range from 2700 to 6500)
+	 */
+	colorTemperature: BehaviorSubject<number>;
+
+	/**
+	 * @returns {BehaviorSubject<number>} Returns RGB color (in RGB Hex)
+	 */
+	rgb: BehaviorSubject<string>;
+
+	/**
+	 * @returns {BehaviorSubject<string>} Returns hue of the device (in degrees 0 - 359)
+	 */
+	hue: BehaviorSubject<number>;
+
+	/**
+	 * @returns {BehaviorSubject<number>} Returns saturation of the device (0 - 100)
+	 */
+	saturation: BehaviorSubject<number>;
+
+	/**
+	 * @returns {BehaviorSubject<YeelightColorModeEnum>} Returns color mode of the device
+	 */
+	colorMode: BehaviorSubject<YeelightColorModeEnum>;
+
+	/**
+	 * Destroys current device and it's socket connection
+	 */
+	destroy: () => void;
+
+	/**
+	 * Set name of the device
+	 * @param {string} name New friendly-name of the device
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	togglePower: () => Promise<IYeelightMethodResponse>;
-	/**
-	 * Adjust brightness, color or temperature of the device (e.g. setting device brighter without knowing it initial state)
-	 */
-	adjust: {
-		/**
-		 * Adjust brightness of the device
-		 * @param {number} difference Number in the range from -100 to 100 (percent)
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		brightness: (difference: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-
-		/**
-		 * Adjust color temperature of the device
-		 * @param {number} difference Number in the range from -100 to 100 (percent)
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		temperature: (difference: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-
-		/**
-		 * Adjust color of the device
-		 * @param {number} difference Number in the range from -100 to 100 (percent)
-		 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
-		 * @param {number} duration Optional parameter with duration (in ms)
-		 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-		 */
-		color: (difference: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
-	};
-}
-
-export interface IYeelight {
-	devices: BehaviorSubject<BehaviorSubject<IYeelightDevice>[]>;
-	/**
-	 * Get device with given name. If there are two or more devices with given name, this function will return only first one.
-	 * @param {string}
-	 * Name of device
-	 * @returns {Observable<BehaviorSubject<IYeelightDevice>>}
-	 * Behaviour subject of device
-	 */
-	getDeviceByName: (name: string) => Observable<BehaviorSubject<IYeelightDevice>>;
+	setName: (name: string) => Promise<IYeelightMethodResponse>;
 
 	/**
-	 * Get device with defined model. If there are two or more devices with given name, this function will return only first one.
-	 * @param {string}
-	 * Name of device
-	 * @returns {Observable<BehaviorSubject<IYeelightDevice>>}
-	 * Behaviour subject of device
-	 */
-	getDeviceByModel: (model: string) => Observable<BehaviorSubject<IYeelightDevice>>;
-}
-
-export interface IYeelightMethods {
-	/**
-	 * Set power state of the device
-	 * @param {IYeelightDevice} device Yeelight device
+	 * Set power of the device
 	 * @param {YeelightPowerState} power `on` or `off`
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	setPower: (device: IYeelightDevice,power: YeelightPowerState,effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>;
+	setPower: (power: YeelightPowerState, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Set color temperature of the device
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {number} colorTemperature Number in the range from 2700 to 6500 (Kelvin)
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	setTemperature: (device: IYeelightDevice,colorTemperature: number,effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>
+	setColorTemperature: (colorTemperature: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Set RGB color of the device
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {string | number | number[]} rgb RGB Color. It can be hex color ("#FFFFFF"), number (16777215) or array of numbers ([255, 255, 255])
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	setRgb: (device: IYeelightDevice,rgb: string | number | number[],effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>
+	setRgb: (rgb: string | number | number[], effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Set hue and saturation of the device
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {number} hue Number in the range from 0 to 359 (degrees)
 	 * @param {number} saturation Number in the range from 0 to 100
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	setHsv: (device: IYeelightDevice,hue: number,saturation: number,effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>
+	setHsv: (hue: number, saturation: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Set brightness of the device
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {number} brightness Number in the range from 0 to 359 (percent)
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	setBrightness: (device: IYeelightDevice,brightness: number,effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>
+	setBrightness: (brightness: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
-	 * Set device state as default
-	 * @param {IYeelightDevice} device Yeelight device
+	 * Set current device state as default
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	setDefault: (device: IYeelightDevice,) => Promise<IYeelightMethodResponse>
+	setAsDefault: () => Promise<IYeelightMethodResponse>;
 
 	/**
-	 * Set name of the device
-	 * @param {IYeelightDevice} device Yeelight device
-	 * @param {string} name New friendly-name of the device
+	 * Toggle current device power state.
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	setName: (device: IYeelightDevice,name: string,) => Promise<IYeelightMethodResponse>
-
-	/**
-	 * Toggle selected device power state.
-	 * @param {IYeelightDevice} device Yeelight device
-	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
-	 */
-	toggle: (device: IYeelightDevice) => Promise<IYeelightMethodResponse>
+	togglePower: () => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Adjust brightness of the device
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {number} difference Number in the range from -100 to 100 (percent)
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	adjustBrightness: (device: IYeelightDevice,difference: number,effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>
+	adjustBrightness: (difference: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Adjust color temperature of the device
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {number} difference Number in the range from -100 to 100 (percent)
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	adjustTemperature: (device: IYeelightDevice,difference: number,effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>
+	adjustTemperature: (difference: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Adjust color of the device
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {number} difference Number in the range from -100 to 100 (percent)
 	 * @param {YeelightEffect} effect Optional parameter with effect - `smooth` or `sudden`
 	 * @param {number} duration Optional parameter with duration (in ms)
 	 * @returns {Promise<IYeelightMethodResponse>} Response with status code (and an error if occured)
 	 */
-	adjustColor: (device: IYeelightDevice,difference: number,effect: YeelightEffect,duration: number) => Promise<IYeelightMethodResponse>
+	adjustColor: (difference: number, effect?: YeelightEffect, duration?: number) => Promise<IYeelightMethodResponse>;
 
 	/**
 	 * Send custom command supported by the device (https://www.yeelight.com/download/Yeelight_Inter-Operation_Spec.pdf)
-	 * @param {IYeelightDevice} device Yeelight device
 	 * @param {id: number, method: string, params: TYeelightParams} command Command that should be send to device.
 	 * @param {(response: IYeelightMethodResponse) => void} callback Callback after operation finished
 	 */
-	sendCommand: (device: IYeelightDevice, command: { id: number, method: string, params: TYeelightParams }, callback: (response: IYeelightMethodResponse) => void) => void
+	sendCommand: (command: { id: number; method: string; params: TYeelightParams }) => Promise<IYeelightMethodResponse>
+}
+
+export interface IYeelight {
+	devices: BehaviorSubject<IYeelightDevice[]>;
+	/**
+	 * Get device with given name. If there are two or more devices with given name, this function will return only first one.
+	 * @param {string}
+	 * Name of device
+	 * @returns {Observable<IYeelightDevice>}
+	 * Behaviour subject of device
+	 */
+	getDeviceByName: (name: string) => Observable<IYeelightDevice>;
+
+	/**
+	 * Get device with defined model. If there are two or more devices with given name, this function will return only first one.
+	 * @param {string}
+	 * Name of device
+	 * @returns {Observable<IYeelightDevice>}
+	 * Behaviour subject of device
+	 */
+	getDeviceByModel: (model: string) => Observable<IYeelightDevice>;
 }
 
 export interface IYeelightMethodResponse {
@@ -350,6 +233,12 @@ export enum YeelightSupportedMethodsEnum {
 	adjustBrightness = 'adjust_bright',
 	adjustTemperature = 'adjust_ct',
 	adjustColor = 'adjust_color',
+}
+
+export enum YeelightColorModeEnum {
+	RGB = 1,
+	COLOR_TEMPERATURE = 2,
+	HSV = 3
 }
 
 export enum YeelightSupportedPropertiesEnum {
